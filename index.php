ef3bd39e749ce2aa152b7928b394da6608f26efb
mhamadkirkuky <m.yahia@ukh.ac>
2015-04-10 20:27:07 -0700

    add

---
+++ index.php
@@ -0,0 +1,215 @@
+<?php
+
+ 
+echo "<head>\n"; 
+echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n"; 
+echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"; 
+echo "<link href=\"css/style.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"; 
+echo "<link href=\"css/responsive.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"; 
+
+echo "<script src=\"js/jquery.min.js\"></script>\n"; 
+echo "<title>Learning Kurdish language</title>\n"; 
+echo "</head>\n"; 
+echo "\n"; 
+echo "<body>\n"; 
+
+
+
+
+
+
+
+
+
+
+echo "    	<div class=\"header\">\n"; 
+echo "        	<div class=\"container\">\n"; 
+echo "            	<div class=\"logo-menu\">\n"; 
+echo "                	<div class=\"logo\">\n"; 
+
+echo "                    	<h1 style=\"font-size:50px\"><strong><br>
+<br>
+<br>
+Learning Kurdish Language</strong></h1>\n"; 
+echo "                    </div>\n"; 
+echo "                    <div class=\"menu\">\n"; 
+echo "                    <label for=\"show-menu\" class=\"show-menu\">Show Menu</label>\n"; 
+echo "					<input type=\"checkbox\" id=\"show-menu\" role=\"button\"></input>\n"; 
+echo "                    	<ul class=\"nav\" id=\"menu\">\n"; 
+echo "                        	<li><a href=\"#\" class=\"active\">Home</a></li>\n"; 
+echo "                            <li><a href=\"#service\">Services</a></li>\n"; 
+echo "                            <li><a href=\"#work\">Our Work</a></li>\n"; 
+echo "                            <li><a href=\"#about\">About</a></li>\n"; 
+echo "                            <li><a href=\"#contact\">Contact</a></li>\n"; 
+
+echo "                        </ul>\n"; 
+echo "                    </div>\n"; 
+echo "                </div>\n"; 
+echo "            </div>\n"; 
+echo "        </div>\n"; 
+
+
+
+echo "    	<div class=\"banner\">\n"; 
+echo "<br>";
+echo "        	<div class=\"container\">\n"; 
+
+echo "            	<div class=\"banner-text\">\n"; 
+
+echo "                	<h2 class=\"tagline\"><strong>Learning Kurdish language in an attractive effective ways:</strong> </h2>\n"; 
+
+echo "                    <p class=\"under-tagline\">This website have been developed for the foreigners and tourism that they want to learn Kurdish languages. The website contain many sections like most common words, sentences, quiz and vocabularies </p>\n"; 
+
+echo "                   <a href='login.php' >  <button class=\"mainbutton\">Log in</button> </a>"; 
+echo "                   <a href='registration.php'> <button class=\"mainbutton\">Sin Up</button></a>\n"; 
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+echo "                </div>\n"; 
+echo "            </div>\n"; 
+echo "        </div>\n"; 
+echo "    <!--------Banner Ends------------>\n"; 
+
+/*------------------------------------------------------------------------------------------------------*/
+
+echo "    <!--------Services Starts-------->\n"; 
+echo "    	<div class=\"services\" id=\"service\">\n"; 
+echo "        	<div class=\"container\">\n"; 
+echo "            	<div class=\"section-head\">\n"; 
+echo "                	<h3>Learning <span>Sections</span></h3>\n"; 
+echo "                    <!--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis faucibus leo. Ut ac arcu a diam mollis semper. In nec blandit nisl, nec cursus lectus.</p>-->\n"; 
+echo "                </div>\n"; 
+echo "                <div class=\"services-list\">\n"; 
+echo "                	<ul>\n"; 
+echo "                		<li>\n"; 
+echo "                    		<div class=\"service-icon icon1\"></div>\n"; 
+echo "                    		<h4>Professional Works</h4>\n"; 
+echo "                    		<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>\n"; 
+echo "                    	</li>\n"; 
+echo "                    	<li>\n"; 
+echo "                    		<div class=\"service-icon icon2\"></div>\n"; 
+echo "                    		<h4>Highly Customizable</h4>\n"; 
+echo "                    		<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>\n"; 
+echo "                    	</li>\n"; 
+echo "                    	<li>\n"; 
+echo "                    		<div class=\"service-icon icon3\"></div>\n"; 
+echo "                    		<h4>Quick Response</h4>\n"; 
+echo "                    	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>\n"; 
+echo "                    	</li>\n"; 
+echo "                	</ul>\n"; 
+echo "                </div>\n"; 
+echo "            </div>\n"; 
+echo "        </div>\n"; 
+echo "    <!--------Services Ends---------->\n"; 
+echo "    \n"; 
+echo "    <!--------Desc Starts------------>\n"; 
+echo "    	<div class=\"desc\">\n"; 
+echo "        	<div class=\"container\">\n"; 
+  
+echo "            </div>\n"; 
+echo "        </div>\n"; 
+echo "    <!--------Desc Ends-------------->\n"; 
+echo "    \n"; 
+echo "    <!--------Work Starts------------>\n"; 
+echo "    	<div class=\"work\" id=\"work\">\n"; 
+echo "        	<div class=\"container\">\n"; 
+echo "            	<div class=\"section-head\">\n"; 
+echo "                	<h3>Sin <span>Up</span></h3>\n"; 
+echo "                </div>\n"; 
+
+
+
+
+
+echo "            		<div class=\"contact-form\">\n"; 
+echo "                		<form>\n"; 
+echo "                    		<input type=\"text\" name=\"Name\" placeholder=\"Name\" />\n"; 
+echo "                        	<input type=\"email\" name=\"email\" placeholder=\"Email\"/>\n"; 
+echo "                        	<textarea placeholder=\"Message\" rows=\"6\"></textarea>\n"; 
+echo "                        	<button type=\"submit\" class=\"submit\">Send Message</button>\n"; 
+echo "                   		</form>\n"; 
+echo "                	</div>\n"; 
+
+
+
+
+
+
+echo "            </div>\n"; 
+echo "        </div>\n"; 
+echo "    <!--------Work Ends-------------->\n"; 
+echo "    \n"; 
+
+
+
+
+
+
+echo "    <!--------About Starts----------->\n"; 
+echo "    	<div class=\"about\" id=\"about\">\n"; 
+echo "        	<div class=\"container\">\n"; 
+echo "            	<div class=\"section-head\">\n"; 
+echo "                	<h3>About <span>Creative Studio</span></h3>\n"; 
+echo "                </div>\n"; 
+echo "                <div class=\"team\">\n"; 
+echo "                    	<ul>\n"; 
+echo "                        	<li><img src=\"images/team-member1.jpg\"/><h5>Angela Leo</h5><span class=\"team-info\">Web developer</span></li>\n"; 
+echo "                            <li><img src=\"images/team-member2.jpg\"/><h5>Angela Leo</h5><span class=\"team-info\">Web developer</span></li>\n"; 
+echo "                            <li><img src=\"images/team-member3.jpg\"/><h5>Angela Leo</h5><span class=\"team-info\">Web developer</span></li>\n"; 
+echo "                            <li><img src=\"images/team-member4.jpg\"/><h5>Angela Leo</h5><span class=\"team-info\">Web developer</span></li>\n"; 
+echo "                        </ul>\n"; 
+echo "                </div>\n"; 
+echo "            </div>\n"; 
+echo "        </div>\n"; 
+echo "    <!--------About Ends------------->\n"; 
+echo "    \n"; 
+echo "    <!--------Contact Starts--------->\n"; 
+echo "    	<div class=\"contact\" id=\"contact\">\n"; 
+echo "        	<div class=\"container\">\n"; 
+echo "            	<div class=\"section-head\">\n"; 
+echo "                	<h3>Get In <span>Touch</span></h3>\n"; 
+echo "                </div>\n"; 
+echo "                <div class=\"contact-section\">\n"; 
+echo "            		<div class=\"contact-form\">\n"; 
+echo "                		<form>\n"; 
+echo "                    		<input type=\"text\" name=\"Name\" placeholder=\"Name\" />\n"; 
+echo "                        	<input type=\"email\" name=\"email\" placeholder=\"Email\"/>\n"; 
+echo "                        	<textarea placeholder=\"Message\" rows=\"6\"></textarea>\n"; 
+echo "                        	<button type=\"submit\" class=\"submit\">Send Message</button>\n"; 
+echo "                   		</form>\n"; 
+echo "                	</div>\n"; 
+echo "                	<div class=\"contact-details\">\n"; 
+echo "                		<p class=\"address\">123689 Pathway, London<br>\n"; 
+echo "                            England\n"; 
+echo "                        </p>\n"; 
+echo "                        <p class=\"tel\">+18965478954</p>\n"; 
+echo "                        <p class=\"mail\">info@exemplary.com</p>\n"; 
+echo "                	</div>\n"; 
+echo "                </div>\n"; 
+echo "            </div>\n"; 
+echo "        </div>\n"; 
+echo "    <!--------Contact Ends----------->\n"; 
+echo "    \n"; 
+echo "    <!--------Footer Starts---------->\n"; 
+echo "    	<div class=\"footer\">\n"; 
+echo "        	<div class=\"container\">\n"; 
+echo "            	<div class=\"infooter\">\n"; 
+echo "                <p class=\"copyright\">developed by Mhamad Saifadeen 2015  </p>\n"; 
+echo "            	</div>\n"; 
+echo "            <ul class=\"socialmedia\">\n"; 
+echo "                <li><a href=\"\"><i class=\"icon-twitter\"></i></a></li>\n"; 
+echo "                <li><a href=\"\"><i class=\"icon-facebook\"></i></a></li>\n"; 
+echo "                <li><a href=\"\"><i class=\"icon-dribbble\"></i></a></li>\n"; 
+echo "                <li><a href=\"\"><i class=\"icon-linkedin\"></i></a></li>\n"; 
+echo "                <li><a href=\"\"><i class=\"icon-instagram\"></i></a></li>\n"; 
+echo "            </ul>\n"; 
+echo "            </div>\n"; 
+echo "        </div>\n"; 
+echo "    <!--------Footer Ends------------>\n"; 
+echo "</body>\n"; 
+echo "</html>\n"; 
+echo "\n";
+?>
